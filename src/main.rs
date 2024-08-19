6e48406c56fe37aa0b2e4ec776cf8beff6773257
soundchaser128 <soundchaser128@gmail.com>
2024-08-19 12:10:29 +0200

    some more features

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     io::Cursor,
     sync::{
         atomic::{AtomicU64, Ordering},
@@ -26,6 +27,7 @@ const MODEL: LazyLock<Model> = LazyLock::new(|| {
     let model = Cursor::new(model);
     create_model(model).expect("failed to create model")
 });
+const METRICS: [Metric; 3] = [Metric::Hentai, Metric::Porn, Metric::Sexy];
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -41,6 +43,10 @@ struct Args {
     #[clap(short = 'd', long = "destination")]
     pub nsfw_folder: Utf8PathBuf,
 
+    /// Flatten the destination folders into a single folder.
+    #[clap(short, long)]
+    pub flatten: bool,
+
     /// Threshold for detecting something as NSFW. Can be between 0 and 1,
     /// 1 being 100% certain that it's NSFW.
     #[clap(short, long, default_value = "0.5")]
@@ -51,7 +57,6 @@ struct Args {
 }
 
 fn is_nsfw(classifications: &[Classification], threshold: f32) -> bool {
-    const METRICS: [Metric; 3] = [Metric::Hentai, Metric::Porn, Metric::Sexy];
     let score_max = classifications
         .iter()
         .filter(|c| METRICS.contains(&c.metric))
@@ -81,6 +86,13 @@ fn collect_paths(source: &Utf8Path) -> Result<Vec<Utf8PathBuf>> {
     Ok(paths)
 }
 
+fn classify_image(path: impl AsRef<Utf8Path>) -> Result<Vec<Classification>> {
+    let image = image::open(path.as_ref())?;
+    let image = image.into_rgba8();
+    let result = examine(&MODEL, &image).map_err(|e| eyre!("failed to examine image: {e}"))?;
+    Ok(result)
+}
+
 fn find_non_conflicting_file_name(dir: &Utf8Path, file_name: &str) -> Result<String> {
     let mut dest = dir.join(file_name);
     if dest.is_file() {
@@ -99,32 +111,6 @@ fn find_non_conflicting_file_name(dir: &Utf8Path, file_name: &str) -> Result<Str
     }
 }
 
-fn copy_image_if_nsfw(
-    path: &Utf8Path,
-    destination_dir: &Utf8Path,
-    dry_run: bool,
-    threshold: f32,
-) -> Result<bool> {
-    let image = image::open(path)?;
-    let image = image.into_rgba8();
-
-    let result = examine(&MODEL, &image).map_err(|e| eyre!("failed to examine image: {e}"))?;
-    let is_nsfw = is_nsfw(&result, threshold);
-
-    if is_nsfw {
-        let file_name = find_non_conflicting_file_name(
-            destination_dir,
-            path.file_name().expect("file must have file name"),
-        )?;
-        let dest = destination_dir.join(file_name);
-        if !dry_run {
-            std::fs::copy(path, dest)?;
-        }
-    }
-
-    Ok(is_nsfw)
-}
-
 fn main() -> Result<()> {
     let args = Args::parse();
     let n_threads = args.num_threads.unwrap_or(num_cpus::get());
@@ -140,18 +126,48 @@ fn main() -> Result<()> {
     println!("found {len} files in {}", args.source_folder);
     let nsfw_count = AtomicU64::new(0);
 
-    image_paths
+    let results: HashMap<_, _> = image_paths
         .into_par_iter()
         .progress_count(len)
-        .for_each(|path| {
-            match copy_image_if_nsfw(&path, &args.nsfw_folder, args.dry_run, args.threshold) {
-                Ok(true) => {
-                    nsfw_count.fetch_add(1, Ordering::SeqCst);
+        .filter_map(|path| {
+            let results = classify_image(&path);
+            match results {
+                Ok(results) => Some((path, results)),
+                Err(e) => {
+                    eprintln!("failed to classify image {path}: {e}");
+                    None
                 }
-                Ok(false) => {}
-                Err(e) => eprintln!("failed to check or move file {path}: {e}"),
             }
-        });
+        })
+        .collect();
+
+    for (path, classifications) in &results {
+        let is_nsfw = is_nsfw(&classifications, args.threshold);
+        if is_nsfw {
+            nsfw_count.fetch_add(1, Ordering::SeqCst);
+            let original_file_name = path.file_name().expect("file must have file name");
+            let dest = if args.flatten {
+                let file_name =
+                    find_non_conflicting_file_name(&args.nsfw_folder, original_file_name)?;
+                args.nsfw_folder.join(file_name)
+            } else {
+                let path_segments = path
+                    .strip_prefix(&args.source_folder)
+                    .expect("must be a prefix");
+                args.nsfw_folder.join(path_segments)
+            };
+
+            if !args.dry_run {
+                println!("Moving '{path}' -> '{dest}'");
+                std::fs::copy(&path, &dest)?;
+            } else {
+                println!("Would move '{path}' -> '{dest}'");
+            }
+        }
+    }
+
+    write_markdown_report(&args.source_folder, &results)?;
+
     let elapsed = start.elapsed();
 
     println!(
@@ -161,6 +177,41 @@ fn main() -> Result<()> {
         args.nsfw_folder
     );
     println!("Elapsed time: {elapsed:?}");
+
+    Ok(())
+}
+
+fn write_markdown_report(
+    source_folder: &Utf8Path,
+    results: &HashMap<Utf8PathBuf, Vec<Classification>>,
+) -> Result<()> {
+    let mut output = String::new();
+    output.push_str("# NSFW Report\n\n");
+    output.push_str(&format!("## Source Folder: {source_folder}\n\n"));
+    // write a markdown table with headers for file path and the different classification types
+    // then write the file path and the classification scores (as percentages) for each image
+    let metrics = METRICS
+        .iter()
+        .map(|m| m.to_string())
+        .collect::<Vec<_>>()
+        .join(" | ");
+    let header = format!("| File | {} |\n", metrics);
+    output += &header;
+
+    let separator = format!("| --- | {:-<1$} |\n", "", metrics.len() * 5);
+    output += &separator;
+
+    for (path, classifications) in results {
+        let classifications = classifications
+            .iter()
+            .map(|c| format!("{:.2}%", c.score * 100.0))
+            .collect::<Vec<_>>()
+            .join(" | ");
+        let row = format!("| {path} | {} |\n", classifications);
+        output += &row;
+    }
+
+    std::fs::write("nsfw_report.md", output)?;
 
     Ok(())
 }
