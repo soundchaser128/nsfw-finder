1cbf4c0f364c729a627e7f872af633df341a20b3
soundchaser128 <soundchaser128@gmail.com>
2024-08-21 18:28:13 +0200

    fix order of table columns being wrong

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    collections::HashMap,
+    collections::BTreeMap,
+    hash::Hash,
     io::Cursor,
     sync::{
         atomic::{AtomicU64, Ordering},
@@ -12,11 +13,7 @@ use camino::{Utf8Path, Utf8PathBuf};
 use clap::Parser;
 use color_eyre::{eyre::eyre, Result};
 use indicatif::ParallelProgressIterator;
-use nsfw::{
-    create_model, examine,
-    model::{Classification, Metric},
-    Model,
-};
+use nsfw::{create_model, examine, model::Metric, Model};
 use ordered_float::OrderedFloat;
 use rayon::prelude::*;
 use walkdir::WalkDir;
@@ -56,15 +53,63 @@ struct Args {
     pub source_folder: Utf8PathBuf,
 }
 
-fn is_nsfw(classifications: &[Classification], threshold: f32) -> bool {
-    let score_max = classifications
-        .iter()
-        .filter(|c| METRICS.contains(&c.metric))
-        .max_by_key(|m| OrderedFloat(m.score))
-        .map(|s| s.score)
-        .unwrap_or(0.0);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum HashableMetric {
+    /// safe for work drawings (including anime)
+    Drawings,
+    /// hentai and pornographic drawings
+    Hentai,
+    /// safe for work neutral images
+    Neutral,
+    /// pornographic images, sexual acts
+    Porn,
+    /// sexually explicit images, not pornography
+    Sexy,
+}
 
-    score_max > threshold
+impl std::fmt::Display for HashableMetric {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let string = match self {
+            HashableMetric::Drawings => "Drawings",
+            HashableMetric::Hentai => "Hentai",
+            HashableMetric::Neutral => "Neutral",
+            HashableMetric::Porn => "Porn",
+            HashableMetric::Sexy => "Sexy",
+        };
+
+        write!(f, "{string}")
+    }
+}
+
+impl From<Metric> for HashableMetric {
+    fn from(metric: Metric) -> Self {
+        match metric {
+            Metric::Drawings => HashableMetric::Drawings,
+            Metric::Hentai => HashableMetric::Hentai,
+            Metric::Neutral => HashableMetric::Neutral,
+            Metric::Porn => HashableMetric::Porn,
+            Metric::Sexy => HashableMetric::Sexy,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct FileResult {
+    pub path: Utf8PathBuf,
+    pub classifications: BTreeMap<HashableMetric, f32>,
+}
+
+impl FileResult {
+    pub fn is_nsfw(&self, threshold: f32) -> bool {
+        let score_max = self
+            .classifications
+            .values()
+            .copied()
+            .max_by_key(|m| OrderedFloat(*m))
+            .unwrap_or(0.0);
+
+        score_max > threshold
+    }
 }
 
 fn collect_paths(source: &Utf8Path) -> Result<Vec<Utf8PathBuf>> {
@@ -86,11 +131,21 @@ fn collect_paths(source: &Utf8Path) -> Result<Vec<Utf8PathBuf>> {
     Ok(paths)
 }
 
-fn classify_image(path: impl AsRef<Utf8Path>) -> Result<Vec<Classification>> {
+fn classify_image(path: impl AsRef<Utf8Path>) -> Result<FileResult> {
     let image = image::open(path.as_ref())?;
     let image = image.into_rgba8();
     let result = examine(&MODEL, &image).map_err(|e| eyre!("failed to examine image: {e}"))?;
-    Ok(result)
+
+    let classifications = result
+        .iter()
+        .filter(|c| METRICS.contains(&c.metric))
+        .map(|c| (HashableMetric::from(c.metric.clone()), c.score))
+        .collect();
+
+    Ok(FileResult {
+        path: path.as_ref().to_owned(),
+        classifications,
+    })
 }
 
 fn find_non_conflicting_file_name(dir: &Utf8Path, file_name: &str) -> Result<String> {
@@ -111,6 +166,46 @@ fn find_non_conflicting_file_name(dir: &Utf8Path, file_name: &str) -> Result<Str
     }
 }
 
+fn write_markdown_report(source_folder: &Utf8Path, results: &[FileResult]) -> Result<()> {
+    let mut output = String::new();
+    output.push_str("# NSFW Report\n\n");
+    output.push_str(&format!("## Source Folder: {source_folder}\n\n"));
+
+    if results.len() > 0 {
+        let metrics = results[0]
+            .classifications
+            .keys()
+            .map(|m| m.to_string())
+            .collect::<Vec<_>>()
+            .join(" | ");
+
+        let header = format!("| File | {} |\n", metrics);
+        output += &header;
+
+        let separator = (0..METRICS.len())
+            .map(|_| "---")
+            .collect::<Vec<_>>()
+            .join(" | ");
+        output += &format!("| --- | {separator} |\n");
+
+        for result in results {
+            let classifications = result
+                .classifications
+                .values()
+                .map(|s| format!("{:.2}%", s * 100.0))
+                .collect::<Vec<_>>()
+                .join(" | ");
+
+            let row = format!("| {} | {classifications} |\n", result.path);
+            output += &row;
+        }
+    }
+
+    std::fs::write("nsfw_report.md", output)?;
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
     let n_threads = args.num_threads.unwrap_or(num_cpus::get());
@@ -126,23 +221,21 @@ fn main() -> Result<()> {
     println!("found {len} files in {}", args.source_folder);
     let nsfw_count = AtomicU64::new(0);
 
-    let results: HashMap<_, _> = image_paths
+    let results: Vec<_> = image_paths
         .into_par_iter()
         .progress_count(len)
-        .filter_map(|path| {
-            let results = classify_image(&path);
-            match results {
-                Ok(results) => Some((path, results)),
-                Err(e) => {
-                    eprintln!("failed to classify image {path}: {e}");
-                    None
-                }
+        .filter_map(|path| match classify_image(&path) {
+            Ok(result) => Some(result),
+            Err(e) => {
+                eprintln!("failed to classify image {path}: {e}");
+                None
             }
         })
         .collect();
 
-    for (path, classifications) in &results {
-        let is_nsfw = is_nsfw(&classifications, args.threshold);
+    for result in &results {
+        let is_nsfw = result.is_nsfw(args.threshold);
+        let path = &result.path;
         if is_nsfw {
             nsfw_count.fetch_add(1, Ordering::SeqCst);
             let original_file_name = path.file_name().expect("file must have file name");
@@ -180,41 +273,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-fn write_markdown_report(
-    source_folder: &Utf8Path,
-    results: &HashMap<Utf8PathBuf, Vec<Classification>>,
-) -> Result<()> {
-    let mut output = String::new();
-    output.push_str("# NSFW Report\n\n");
-    output.push_str(&format!("## Source Folder: {source_folder}\n\n"));
-    // write a markdown table with headers for file path and the different classification types
-    // then write the file path and the classification scores (as percentages) for each image
-    let metrics = METRICS
-        .iter()
-        .map(|m| m.to_string())
-        .collect::<Vec<_>>()
-        .join(" | ");
-    let header = format!("| File | {} |\n", metrics);
-    output += &header;
-
-    let separator = format!("| --- | {:-<1$} |\n", "", metrics.len() * 5);
-    output += &separator;
-
-    for (path, classifications) in results {
-        let classifications = classifications
-            .iter()
-            .map(|c| format!("{:.2}%", c.score * 100.0))
-            .collect::<Vec<_>>()
-            .join(" | ");
-        let row = format!("| {path} | {} |\n", classifications);
-        output += &row;
-    }
-
-    std::fs::write("nsfw_report.md", output)?;
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {}
